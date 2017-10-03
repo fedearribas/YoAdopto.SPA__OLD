180e2124228d719dd25a437d9cf86df335cd2026
Federico <fede.arribas@gmail.com>
2017-10-02 21:23:39 -0300

    node server config

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ const forceSSL = function() {
 app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist', { index: 'index.html' }));
+//app.use(express.static(__dirname + '/dist', { index: 'index.html' }));
+app.use(express.static(path.join(__dirname + 'dist/index.html')));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
