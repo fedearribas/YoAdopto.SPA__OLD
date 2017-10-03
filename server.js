0c2438aaa4f92dc9017e907efd38c35785e740d3
Federico <fede.arribas@gmail.com>
2017-10-02 21:26:01 -0300

    node server config

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
 //app.use(express.static(__dirname + '/dist', { index: 'index.html' }));
-app.use(express.static(path.join(__dirname + 'dist/index.html')));
+app.use(express.static(path.join(__dirname + '/dist/index.html')));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
