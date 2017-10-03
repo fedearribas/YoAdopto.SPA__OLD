269c780dd4234621d805ae0f596e8d1a889d1fa7
Federico <fede.arribas@gmail.com>
2017-10-02 21:29:54 -0300

    node server config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var staticRoot = __dirname;
 // If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
@@ -21,7 +22,10 @@ app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
 //app.use(express.static(__dirname + '/dist', { index: 'index.html' }));
-app.use(express.static(path.join(__dirname + '/dist/index.html')));
+app.use(express.static(staticRoot));
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+});
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
